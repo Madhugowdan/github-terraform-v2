506efa62a92cd2c26a3afcb6bdcf30ef8141f44f
madhuUser1 <madhungowda6590@gmail.com>
2023-01-25 03:18:50 +0100

    vv2

--- terraform-code/provider.tf
+++ terraform-code/provider.tf
@@ -12,8 +12,8 @@
 
 provider "aws" {
   region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  # access_key = var.access_key
+  # secret_key = var.secret_key
 
 }
 
